7cafa51ff0ace39ddce52630a33d4157c30d3e16
joao_cc <joao_cc@yahoo.com>
2024-06-04 10:49:17 +0200

    setup allauth

--- toolboxcorner/urls.py
+++ toolboxcorner/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('', include('website.urls')),
     path('products/', include('website.urls')),
     path('contact/', include('contact.urls')),

d9cc686bb4dd3ee79cbb40fa9a9f67aa58ce6080
joao_cc <joao_cc@yahoo.com>
2024-06-05 13:37:27 +0200

    add bag context and delivery logic and categories context

--- toolboxcorner/settings.py
+++ toolboxcorner/settings.py
@@ -76,6 +76,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
+                'website.contexts.categories',
             ],
         },
     },
@@ -169,3 +171,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 20
+STANDARD_DELIVERY_PERCENTAGE = 10

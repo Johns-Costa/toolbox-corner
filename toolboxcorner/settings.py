e6b68482519f7c2d6777803558d1a7b59666988d
joao_cc <joao_cc@yahoo.com>
2024-06-13 21:08:20 +0200

    debug to false

--- toolboxcorner/settings.py
+++ toolboxcorner/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['toolbox-corner-c9446b25bd36.herokuapp.com', 'localhost', '127.0.0.1']
 

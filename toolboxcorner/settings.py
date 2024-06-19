0a5d7fcec28e93d2c21ae88d066ad5fbd8c8d714
João Campos Costa <joao_cc@yahoo.com>
2024-06-19 19:50:40 +0000

    update requirements.txt

--- toolboxcorner/settings.py
+++ toolboxcorner/settings.py
@@ -70,7 +70,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    
 ]
 
 ROOT_URLCONF = 'toolboxcorner.urls'

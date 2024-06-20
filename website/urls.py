53face9882dce687d57fd916c0c03f017c73e34c
João Campos Costa <joao_cc@yahoo.com>
2024-06-20 15:45:08 +0000

    create delete product template and fix some css

--- website/urls.py
+++ website/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('product/add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', edit_product, name='edit_product'),
-    path('product/delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
     path('bag/', include('bag.urls')),
     path('search/', search_results, name='search_results'),
 ]

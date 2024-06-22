6fa6464aa841ebd3ad3e4a3051334caa75ff2daf
João Campos Costa <joao_cc@yahoo.com>
2024-06-22 14:47:42 +0000

    add footer and fix images in medium and small screen

--- toolboxcorner/urls.py
+++ toolboxcorner/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 handler404 = 'toolboxcorner.views.handler404'
